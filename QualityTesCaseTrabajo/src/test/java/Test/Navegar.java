5b70f462b14087d6b44d66764d119fa7b9c1a890
anderson4400 <anderson440040@gmail.com>
2022-03-14 15:20:57 -0400

    Test1

---
+++ QualityTesCaseTrabajo/src/test/java/Test/Navegar.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Navegar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Driver h = new Driver();
+		h.driver();
+	}
+
+}
